ce44f9ce57b4fc1b76a202eb982307a263951edd
krishna kumar <84628880+gitkrishnaa@users.noreply.github.com>
2023-06-04 14:03:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require("express")
 const app=express()
 app.get("/",(req,res)=>{
     res.send("hello krishna")
-    console
+    console.log("server started at port 3000")
 })
-
+console.log("index.js working fine")
 app.listen(3000)
